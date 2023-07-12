5d520c92c4d4c5e9991b6c9e9b62a23d408b6529
zuhair <zuhair.hamzah08@gmail.com>
2023-07-12 19:52:49 +0800

    boolean concept

---
+++ boolean.rb
@@ -0,0 +1,29 @@
+# assignment / penugasan
+a = 1
+b = 2
+
+a == b #false
+a > b #false
+a < b #true
+a >= b #false
+a <= b # true
+a != b # true
+
+# AND
+true && true # true
+true && false # false
+false && true # false
+false && false # false
+
+# OR
+true || true # true
+true || false # true
+false || true # true
+false || false # false
+
+# Spaceship Operator yang disebut comparison operator memiliki 3 kemungkinan nilai
+1 <=> 2 # -1
+1 <=> 2 # 0
+1 <=> 2 # 1
+
+1 <=> "a" # nill , non comparable
